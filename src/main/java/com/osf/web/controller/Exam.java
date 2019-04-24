5fb83bcbea5febaea9247f2214e7c303c6b533e7
Qum92 <oscar9992@naver.com>
2019-04-24 16:04:50 +0900

    2019.04.24

--- src/main/java/com/osf/web/controller/Exam.java
+++ src/main/java/com/osf/web/controller/Exam.java
@@ -6,17 +6,17 @@ import org.slf4j.LoggerFactory;
 import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Exam {
-	private final static Logger logger = LoggerFactory.getLogger(Exam.class);
-	public String str="11";
-
-	public static void main(String[] args) {
-		Exam e1 = new Exam();
-		String str = null;
-		if (str.equals("홍길동")) {
-			log.info("이름은 홍길동이 맞습니다.");
-		}else {
-			log.info("이름은 홍길동이 아닙니다.");
-		}
-		log.warn("프로그램 비정상종료");
-	}
+//	private final static Logger logger = LoggerFactory.getLogger(Exam.class);
+//	public String str="11";
+//
+//	public static void main(String[] args) {
+//		Exam e1 = new Exam();
+//		String str = null;
+//		if (str.equals("홍길동")) {
+//			log.info("이름은 홍길동이 맞습니다.");
+//		}else {
+//			log.info("이름은 홍길동이 아닙니다.");
+//		}
+//		log.warn("프로그램 비정상종료");
+//	}
 }
---
+++ src/main/java/com/osf/web/test/NullTest.java
@@ -0,0 +1,20 @@
+package com.osf.web.test;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class NullTest {
+
+   public static void main(String[] args) {
+
+      String test="test";
+
+      if(test.equals("test")) {
+
+         log.info("test => {}",test);
+
+      }
+
+   }
+
+}
